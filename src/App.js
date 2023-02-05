d8a0e35d729443265367d74c9d710721a47a46c8
Gurkirat <63462904+SshgurkiratSingh@users.noreply.github.com>
2023-02-05 17:24:29 +0530

    CRONE

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./Component/Navbar";
+function App() {
+  return (
+    <div>
+      <h1>hi</h1>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;

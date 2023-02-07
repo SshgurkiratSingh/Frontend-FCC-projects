aa8d722eb8ec93f1b9e9bbe7a198caf51c399354
Gurkirat <63462904+SshgurkiratSingh@users.noreply.github.com>
2023-02-07 07:41:37 +0530

    crone:

--- src/pages/DrumProject/index.js
+++ src/pages/DrumProject/index.js
@@ -4,6 +4,12 @@ import { useState } from "react";
 import Drum from "../../Component/Drum";
 import clap from "./sounds/clap.wav";
 import heater1 from "./sounds/Heater-1.mp3";
+import heater2 from "./sounds/Heater-2.mp3";
+import heater3 from "./sounds/Heater-3.mp3";
+import kick from "./sounds/kick.wav";
+import openhat from "./sounds/openhat.wav";
+import ride from "./sounds/ride.wav";
+import snare from "./sounds/snare.wav";
 
 function DrumProject() {
   const [sound, setSound] = useState([
@@ -17,20 +23,48 @@ function DrumProject() {
       sound: heater1,
       key: "W",
     },
+    {
+      name: "heater-2",
+      sound: heater2,
+      key: "E",
+    },
+    {
+      name: "heater-3",
+      sound: heater3,
+      key: "A",
+    },
+    {
+      name: "kick",
+      sound: kick,
+      key: "D",
+    },
+    {
+      name: "openhat",
+      sound: openhat,
+      key: "Z",
+    },
+    {
+      name: "snare",
+      sound: snare,
+      key: "X",
+    },
+    {
+      name: "Ride",
+      sound: ride,
+      key: "C",
+    },
   ]);
   const [text, setText] = useState("Press a Button or a related key");
 
   return (
     <>
       {" "}
-      <center>
-        <h1 className="block font-extrabold text-5xl translate-y-12">
-          React Drum Project
-        </h1>
-        <div id="drum-machine">
-          <div id="display" className="display">
-            {text}
-          </div>
+      <h1 className="block font-extrabold text-5xl ">React Drum Project</h1>
+      <div id="drum-machine">
+        <div id="display" className="display content-center	">
+          {text}
+        </div>
+        <div className="place-content-center grid ">
           {sound.map((s) => (
             <Drum
               key={s.key}
@@ -41,7 +75,7 @@ function DrumProject() {
             />
           ))}
         </div>
-      </center>
+      </div>
     </>
   );
 }
